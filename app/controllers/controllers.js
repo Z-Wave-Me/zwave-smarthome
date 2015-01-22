dcb7e7e93e748dc469dfcd485ccefab909203519
Martin Vach <mv@zwaveeurope.com>
2015-01-22 09:49:34 +0100

    Zwave oprn weather implementation

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -151,6 +151,9 @@ myAppController.controller('BaseController', function($scope, $cookies, $filter,
  * Test controller
  */
 myAppController.controller('TestController', function($scope, $routeParams, $filter, $location, dataFactory, dataService) {
+    
+    $scope.myData = 'Niels';
+     $scope.show = true;
 });
 /**
  * Home controller
