45e33b52ffa2be3cc69f47c4f17c9091c4d8fd2f
Martin Vach <mv@zwaveeurope.com>
2015-05-07 12:11:41 +0200

    Elements - close popup window fixed

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -142,6 +142,8 @@ myAppController.controller('TestController', function($scope, $routeParams, $fil
  */
 myAppController.controller('ElementController', function($scope, $routeParams, $interval, dataFactory, dataService, myCache) {
     $scope.isValidUser();
+    $scope.goHidden = [];
+    $scope.goHistory = [];
     $scope.apiDataInterval = null;
     $scope.collection = [];
     $scope.showFooter = true;
