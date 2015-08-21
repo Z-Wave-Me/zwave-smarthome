3b1179eb9b75cd163b9a5139cd2213a10bca526a
Martin Vach <mv@zwaveeurope.com>
2015-08-21 11:51:57 +0200

    Network/Z-Wave devices - deprecated code removed

--- app/controllers/network.js
+++ app/controllers/network.js
@@ -49,137 +49,6 @@ myAppController.controller('NetworkController', function($scope, $cookies, $filt
     };
     $scope.loadData();
 
-    /**
-     * DEPRECATED
-     * Assign devices to room
-     */
-//    $scope.devicesToRoom = function(roomId, devices) {
-//        if (!roomId) {
-//            return;
-//        }
-//        $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('updating')};
-//        for (var i = 0; i <= devices.length; i++) {
-//            var v = devices[i];
-//            if (!v) {
-//                continue;
-//            }
-//            var input = {
-//                id: v.id,
-//                location: roomId
-//            };
-//            dataFactory.putApi('devices', v.id, input).then(function(response) {
-//            }, function(error) {
-//                alert($scope._t('error_update_data'));
-//                $scope.loading = false;
-//                dataService.logError(error);
-//                return;
-//            });
-//        }
-//        myCache.remove('devices');
-//        $scope.loadData();
-//        $scope.loading = false;
-//        return;
-//
-//    };
-
-    /**
-     * DEPRECATED
-     * Set device visibility
-     */
-//    $scope.setVisibility = function(deviceId, visibility) {
-//        var input = {
-//            id: deviceId,
-//            visibility: visibility
-//        };
-//
-//        $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('updating')};
-//        dataFactory.putApi('devices', deviceId, input).then(function(response) {
-//            myCache.remove('devices');
-//            $scope.loadData();
-//            $scope.loading = false;
-//        }, function(error) {
-//            alert($scope._t('error_update_data'));
-//            $scope.loading = false;
-//            dataService.logError(error);
-//        });
-//
-//    };
-
-    /**
-     * DEPRECATED
-     * Set device visibility
-     */
-//    $scope.renameDevice = function(deviceId, title) {
-//        var input = {
-//            id: deviceId,
-//            metrics: {
-//                title: title
-//            }
-//        };
-//
-//        $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('updating')};
-//        dataFactory.putApi('devices', deviceId, input).then(function(response) {
-//            myCache.remove('devices');
-//            $scope.loadData();
-//            $scope.loading = false;
-//        }, function(error) {
-//            alert($scope._t('error_update_data'));
-//            $scope.loading = false;
-//            dataService.logError(error);
-//        });
-//
-//    };
-
-    /**
-     * DEPRECATED
-     * Add/Remove device in list
-     */
-//    $scope.hiddenList = function(deviceId, checked) {
-//        if (checked) {
-//            if ($scope.hiddenDevices.indexOf(deviceId) === -1) {
-//                $scope.hiddenDevices.push(deviceId);
-//            }
-//        } else {
-//            for (var i = 0; i <= $scope.hiddenDevices.length; i++) {
-//                var v = $scope.hiddenDevices[i];
-//                if (v === deviceId) {
-//                    $scope.hiddenDevices.splice(i, 1);
-//                }
-//            }
-//        }
-//    };
-
-    /**
-     * DEPRECATED
-     * Update devices with status hidden
-     */
-//    $scope.handleHidden = function() {
-//        var devices = [];
-//        for (var i = 0; i <= $scope.devices.zwave.length; i++) {
-//            var v = $scope.devices.zwave[i];
-//            if(!v){
-//                continue;
-//            }
-//            var isHidden = false;
-//            if ($scope.hiddenDevices.indexOf(v.id) !== -1) {
-//                isHidden = true;
-//            }
-//            devices[v.id] = isHidden;
-//        }
-//
-//         $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('updating')};
-//            dataFactory.postApi('hide_devices', {data: devices}).then(function(response) {
-//                  myCache.remove('devices');
-//                   $scope.loadData();
-//                   $scope.loading = false;
-//            }, function(error) {
-//                alert($scope._t('error_update_data'));
-//                $scope.loading = false;
-//                dataService.logError(error);
-//            });
-//
-//    };
-
     /// --- Private functions --- ///
     /**
      * Load locations
@@ -233,37 +102,13 @@ myAppController.controller('NetworkController', function($scope, $cookies, $filt
                         var interviewDone = isInterviewDone(node, nodeId);
                         var isFailed = node.data.isFailed.value;
                         var hasBattery = 0x80 in node.instances[0].commandClasses;
-                        // Has config file
-//                        if (angular.isDefined(node.data.ZDDXMLFile) && node.data.ZDDXMLFile.value != '') {
-//                            if ($scope.zWaveDevices[nodeId]['cfg'].indexOf('config') === -1) {
-//                                $scope.zWaveDevices[nodeId]['cfg'].push('config');
-//                            }
-//                        }
-//                        // Has wakeup
-//                        if (0x84 in node.instances[0].commandClasses) {
-//                            if ($scope.zWaveDevices[nodeId]['cfg'].indexOf('wakeup') === -1) {
-//                                $scope.zWaveDevices[nodeId]['cfg'].push('wakeup');
-//                            }
-//                        }
-//                        // Has SwitchAll
-//                        if (0x27 in node.instances[0].commandClasses) {
-//                            if ($scope.zWaveDevices[nodeId]['cfg'].indexOf('switchall') === -1) {
-//                                $scope.zWaveDevices[nodeId]['cfg'].push('switchall');
-//                            }
-//                        }
-//                        // Has protection
-//                        if (0x75 in node.instances[0].commandClasses) {
-//                            if ($scope.zWaveDevices[nodeId]['cfg'].indexOf('protection') === -1) {
-//                                $scope.zWaveDevices[nodeId]['cfg'].push('protection');
-//                            }
-//                        }
                         var obj = {};
                         obj['id'] = v.id;
                         obj['visibility'] = v.visibility;
                         obj['permanently_hidden'] = v.permanently_hidden;
                         obj['nodeId'] = nodeId;
                         obj['nodeName'] = node.data.givenName.value || 'Device ' + '_' + k,
-                                obj['title'] = v.metrics.title;
+                        obj['title'] = v.metrics.title;
                         obj['deviceType'] = v.deviceType;
                         obj['level'] = $filter('toInt')(v.metrics.level);
                         obj['metrics'] = v.metrics;
