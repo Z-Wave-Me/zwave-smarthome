daa4e044759caa8d1694232525e4b13fdcd38c71
Martin Vach <mv@zwaveeurope.com>
2015-11-04 15:52:05 +0100

    Elements: load data if structureChanged

--- app/controllers/element.js
+++ app/controllers/element.js
@@ -219,6 +219,10 @@ myAppController.controller('ElementController', function($scope, $routeParams, $
             dataFactory.refreshApi('devices').then(function(response) {
                 dataService.updateDevices(response.data);
                 dataService.updateTimeTick(response.data.data.updateTime);
+                if(response.data.data.structureChanged === true){
+                      $scope.loadData();
+                }
+               
             }, function(error) {
                 dataService.showConnectionError(error);
             });
