cfd7781eb2e4175ae0d7e613b600c490005bb4c0
Martin Vach <mv@zwaveeurope.com>
2015-12-01 10:17:43 +0100

    DIST

--- dist/app/js/build.js
+++ dist/app/js/build.js
@@ -8518,6 +8518,14 @@ myApp.config(['$routeProvider', function($routeProvider) {
                     templateUrl: 'app/views/auth/password.html',
                     requireLogin: true
                 }).
+                //Password forgot
+                when('/passwordforgot', {
+                    templateUrl: 'app/views/auth/password_forgot.html'
+                }).
+                //Password reset
+                when('/passwordforgot/reset/:token', {
+                    templateUrl: 'app/views/auth/password_reset.html'
+                }).
                 //Login
                 when('/logout', {
                     templateUrl: 'app/views/auth/logout.html',
@@ -15535,9 +15543,13 @@ myAppController.controller('EnoceanManageController', function($scope, $location
      * Delete device
      */
     $scope.deleteDevice = function(id, target, message) {
-        var cmd = 'delete devices["' + id + '"]';
+        var cmd = 'delete devices["x' + id + '"]';
         alertify.confirm(message, function() {
             dataFactory.runEnoceanCmd(cmd).then(function(response) {
+                if(response.data === 'false'){
+                    alertify.alert($scope._t('error_delete_data'));
+                    return;
+                }
                 $(target).fadeOut(500);
                 //$scope.loadData();
             }, function(error) {
@@ -17042,21 +17054,7 @@ myAppController.controller('ManagementInfoController', function($scope, dataFact
 myAppController.controller('MySettingsController', function($scope, $window, $location,$cookies,dataFactory, dataService, myCache) {
     $scope.id = $scope.user.id;
     $scope.devices = {};
-    $scope.input = {
-        id: 0,
-        name: '',
-        active: true,
-        description: '',
-        //positions: [],
-        password: '',
-        lang: 'en',
-        color: '',
-        hide_all_device_events: false,
-        hide_system_events: false,
-        hide_single_device_events: [],
-        interval: 2000
-
-    };
+    $scope.input = {};
     $scope.newPassword = null;
 
     /**
@@ -17174,6 +17172,7 @@ myAppController.controller('MySettingsController', function($scope, $window, $lo
     ;
 
 });
+
 /**
  * Application Auth controller
  * @author Martin Vach
@@ -17184,7 +17183,7 @@ myAppController.controller('MySettingsController', function($scope, $window, $lo
 /**
  * Login controller
  */
-myAppController.controller('LoginController', function($scope, $location, $window, $routeParams, $cookies,dataFactory, dataService) {
+myAppController.controller('LoginController', function($scope, $location, $window, $routeParams, $cookies, dataFactory, dataService) {
     $scope.input = {
         form: true,
         login: '',
@@ -17211,22 +17210,22 @@ myAppController.controller('LoginController', function($scope, $location, $windo
         dataFactory.logInApi(input).then(function(response) {
             var redirectTo = '#/elements/dashboard/1?login';
             var user = response.data.data;
-             if($scope.loginLang){
-                 user.lang = $scope.loginLang;
-             }
+            if ($scope.loginLang) {
+                user.lang = $scope.loginLang;
+            }
             dataService.setZWAYSession(user.sid);
             dataService.setUser(user);
             dataService.setLastLogin(Math.round(+new Date() / 1000));
             //$scope.loading = false;
             $scope.input.form = false;
             //$window.location.href = '#/elements/dashboard/1?login';
-             //console.log(user);
+            //console.log(user);
             //$location.path('/elements/dashboard/1?login');
-            if(input.password === $scope.cfg.default_credentials.password){
+            if (input.password === $scope.cfg.default_credentials.password) {
                 redirectTo = '#/password';
             }
             window.location = redirectTo;
-           
+
             $window.location.reload();
         }, function(error) {
             var message = $scope._t('error_load_data');
@@ -17248,21 +17247,21 @@ myAppController.controller('LoginController', function($scope, $location, $windo
 /**
  * Password controller
  */
-myAppController.controller('PasswordController', function($scope,  dataFactory) {
+myAppController.controller('PasswordController', function($scope, dataFactory) {
     //$scope.newPassword = null;
     $scope.input = {
         password: '',
-       passwordConfirm: '',
-       email: '',
+        passwordConfirm: '',
+        email: ''
     };
     /**
      * Change password
      */
-    $scope.changePassword = function(form,input) {
+    $scope.changePassword = function(form, input) {
         if (form.$invalid) {
             return;
         }
-        if (input.password === '' || input.password === $scope.cfg.default_credentials.password) {
+        if (input.password === $scope.cfg.default_credentials.password) {
             alertify.alert($scope._t('enter_valid_password'));
             $scope.loading = false;
             return;
@@ -17274,7 +17273,53 @@ myAppController.controller('PasswordController', function($scope,  dataFactory) 
             email: input.email
 
         };
-         dataFactory.putApi('profiles_auth_update', input.id, input).then(function(response) {
+        dataFactory.putApi('profiles_auth_update', input.id, input).then(function(response) {
+            var data = response.data.data;
+            data['email'] = input.email;
+            if (!data) {
+                alertify.alert($scope._t('error_update_data'));
+                $scope.loading = false;
+                return;
+            }
+            dataFactory.putApi('profiles', input.id, data).then(function(response) {}, function(error) {});
+            $scope.loading = {status: 'loading-fade', icon: 'fa-check text-success', message: $scope._t('success_updated')};
+            window.location = '#/elements/dashboard/1';
+
+        }, function(error) {
+            alertify.alert($scope._t('error_update_data'));
+            $scope.loading = false;
+        });
+        
+        
+        
+
+    };
+
+});
+/**
+ * Password forgot controller
+ */
+myAppController.controller('PasswordForgotController', function($scope, $location,dataFactory) {
+    $scope.passwordForgot = {
+        input: { email: '',location: $location,resetUrl: $location.$$absUrl + '/reset/'},
+        alert: {message: false, status: 'is-hidden', icon: false}
+    };
+
+    /**
+     * Send an email
+     */
+    $scope.sendEmail = function(form, input) {
+        if (form.$invalid) {
+            return;
+        }
+        $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('loading')};
+        alertify.alert($scope._t('email_notfound'));
+         $scope.passwordForgot.alert = {message: $scope._t('password_forgot_success'), status: 'alert-success', icon: 'fa-check'};
+        $scope.loading = false;
+
+        return;
+
+        dataFactory.putApi('profiles_auth_update', input.id, input).then(function(response) {
             var data = response.data.data;
             if (!data) {
                 alertify.alert($scope._t('error_update_data'));
@@ -17282,7 +17327,77 @@ myAppController.controller('PasswordController', function($scope,  dataFactory) 
                 return;
             }
             $scope.loading = {status: 'loading-fade', icon: 'fa-check text-success', message: $scope._t('success_updated')};
-             window.location = '#/elements/dashboard/1';
+            window.location = '#/elements/dashboard/1';
+
+        }, function(error) {
+            alertify.alert($scope._t('error_update_data'));
+            $scope.loading = false;
+        });
+
+    };
+
+});
+
+/**
+ * Password reset controller
+ */
+myAppController.controller('PasswordResetController', function($scope, $routeParams,dataFactory) {
+   $scope.passwordReset = {
+        input: { id: null, password: '',passwordConfirm: '',token: $routeParams.token},
+        alert: {message: false, status: 'is-hidden', icon: false}
+    };
+    /**
+     * Check a valid token
+     */
+    $scope.checkToken = function(token) {
+        $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('loading')};
+         $scope.passwordReset.input.id = 1;
+         $scope.loading = false;
+         return;
+
+        dataFactory.getApi('myappi', null, true).then(function(response) {
+            $scope.passwordReset.input.id = response.data.data.user.id;
+            $scope.passwordReset.input.id = 1;
+            $scope.loading = false;
+        }, function(error) {
+            var message = $scope._t('error_500');
+            if (error.status == 404) {
+                message = $scope._t('token_notfound');
+            }
+            $scope.loading = false;
+            $scope.passwordReset.alert = {message: message, status: 'alert-danger', icon: 'fa-warning'};
+        });
+
+    };
+    $scope.checkToken($routeParams.token);
+    
+    /**
+     * Change password
+     */
+    $scope.changePassword = function(form, input) {
+        if (form.$invalid) {
+            return;
+        }
+        if (input.password === '' || input.password === $scope.cfg.default_credentials.password) {
+            alertify.alert($scope._t('enter_valid_password'));
+            $scope.loading = false;
+            return;
+        }
+        $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('updating')};
+        var input = {
+            id: input.id,
+            password: input.password
+
+        };
+        dataFactory.putApi('profiles_auth_update', input.id, input).then(function(response) {
+            var data = response.data.data;
+            if (!data) {
+                alertify.alert($scope._t('error_update_data'));
+                $scope.loading = false;
+                return;
+            }
+            $scope.loading = {status: 'loading-fade', icon: 'fa-check text-success', message: $scope._t('success_updated')};
+            window.location = '#/';
 
         }, function(error) {
             alertify.alert($scope._t('error_update_data'));
