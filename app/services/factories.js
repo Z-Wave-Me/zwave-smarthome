e97e8d9742502e2978590ff065b38a3b7905d223
Maros Kollar <maros@k-1.com>
2015-12-08 16:40:04 +0100

    Add session and logout api endpoints

--- app/services/factories.js
+++ app/services/factories.js
@@ -31,6 +31,8 @@ myAppFactory.factory('dataFactory', function($http, $filter, $q, myCache, dataSe
     }
     return({
         logInApi: logInApi,
+        logOutApi: logOutApi,
+        sessionApi: sessionApi,
         getApiLocal: getApiLocal,
         getApi: getApi,
         deleteApi: deleteApi,
@@ -74,6 +76,32 @@ myAppFactory.factory('dataFactory', function($http, $filter, $q, myCache, dataSe
             method: "post",
             data: data,
             url: cfg.server_url + cfg.api['login']
+        }).then(function(response) {
+            return response;
+        }, function(response) {// something went wrong
+            //return response;
+            return $q.reject(response);
+        });
+    }
+    
+    // Get api data
+    function logOutApi() {
+        return $http({
+            method: "get",
+            url: cfg.server_url + cfg.api['logout']
+        }).then(function(response) {
+            return response;
+        }, function(response) {// something went wrong
+            //return response;
+            return $q.reject(response);
+        });
+    }
+    
+    // Get api data
+    function sessionApi() {
+        return $http({
+            method: "get",
+            url: cfg.server_url + cfg.api['session']
         }).then(function(response) {
             return response;
         }, function(response) {// something went wrong
