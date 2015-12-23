ca45a1445957791a8ba2962341056ccbac5fb878
Martin Vach <mv@zwaveeurope.com>
2015-12-23 14:41:33 +0100

    Removing sass-cache dir from project

--- app/css/config.rb
+++ app/css/config.rb
@@ -6,6 +6,7 @@ css_dir = "/"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+cache_path = '../../../_tmp/.sass-cache'
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
