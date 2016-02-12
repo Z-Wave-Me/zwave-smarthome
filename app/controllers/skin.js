e337aad7898f12056e6f1b31c7321478ecebd06e
Martin Vach <mv@zwaveeurope.com>
2016-02-12 16:36:30 +0100

    Updating

--- app/controllers/skin.js
+++ app/controllers/skin.js
@@ -6,12 +6,13 @@
 /**
  * Skin base controller
  */
-myAppController.controller('SkinBaseController', function ($scope, dataFactory, dataService) {
+myAppController.controller('SkinBaseController', function ($scope, dataFactory, _) {
     $scope.skins = {
         local: {
             all: {},
             find: {},
-            ids: {}
+            ids: {},
+            show: false
         },
         online: {
             all: {},
@@ -22,6 +23,29 @@ myAppController.controller('SkinBaseController', function ($scope, dataFactory, 
             all: {}
         }
     };
+
+    /**
+     * Load z-wave devices
+     */
+    $scope.loadLocalSkins = function () {
+        $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('loading')};
+        dataFactory.getApiLocal('skins.json').then(function (response) {
+            $scope.loading = false;
+            $scope.skins.local.all = _.chain(response.data.data)
+                    .flatten()
+                    .filter(function (v) {
+                        v.icon = (!v.icon ? 'storage/img/placeholder-img.png' :'storage/skins/' + v.icon );
+                        return v;
+                    })
+                    .value();
+            ;
+            $scope.skins.local.show = true;
+        }, function (error) {
+            $scope.loading = false;
+            alertify.alertError($scope._t('error_load_data'));
+        });
+    };
+    $scope.loadLocalSkins();
 
 });
 
