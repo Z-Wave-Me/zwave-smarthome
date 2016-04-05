8daf1dfe64c3a4d548b74a34a409a7450189cb1a
Martin Vach <mv@zwaveeurope.com>
2016-04-05 15:46:42 +0200

    Version update

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@ var config_data = {
         //Application name
         'app_name': 'SmartHome UI',
         // Application version
-        'app_version': '1.3.1',
+        'app_version': '1.4.0',
         // Server base url
         'server_url': '/',
         //'server_url': 'http://192.168.10.119:8083/',
