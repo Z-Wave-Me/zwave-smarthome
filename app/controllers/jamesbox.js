4d9de184d3b4d6770e37c28431fca98e21e046da
Martin Vach <mv@zwaveeurope.com>
2016-04-14 15:26:06 +0200

    JamesBox: updating

--- app/controllers/jamesbox.js
+++ app/controllers/jamesbox.js
@@ -6,25 +6,41 @@
 /**
  * Skin base controller
  */
-myAppController.controller('JbUpdateController', function ($scope, $timeout, dataFactory, dataService, _) {
+myAppController.controller('JbUpdateController', function ($scope, $timeout, $location, dataFactory, dataService, _) {
     $scope.jamesbox = {
-        proccess: false
+        proccess: false,
+        success: false
     };
 
     /**
      * Update firmware
      */
-    $scope.updateFirmware = function () {
+    $scope.firmwareUpdate = function () {
         $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('updating')};
         dataFactory.getApi('jb_update').then(function (response) {
             $timeout(function () {
                 $scope.loading = false;
-                dataService.showNotifier({message: 'Firmware successfully updated'});
-            }, 3000);
+                alertify.alertWarning('The firmware will be automatically upgraded to version XXX on next restart.')
+                        .set('onok', function (closeEvent) {
+                            alertify.dismissAll();
+                            $location.path("/dashboard");
+                        });
+            }, 5000);
         }, function (error) {
-            alertify.alertError($scope._t('error_update_data'));
+            alertify.alertError($scope._t('Something went wrong. Please restart this process.'));
             $scope.loading = false;
         });
+    };
+
+    /**
+     * Cancel firmware update
+     */
+    $scope.firmwareCancel = function () {
+        alertify.alertWarning('The firmware will be automatically upgraded to version XXX on next restart.')
+                .set('onok', function (closeEvent) {
+                    alertify.dismissAll();
+                    $location.path("/login");
+                });
     };
 
 });
