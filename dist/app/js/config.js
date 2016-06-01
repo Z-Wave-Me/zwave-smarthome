ba9873fcd582c265ca9959b3e61c4ba9f3d994f2
Martin Vach <mv@zwaveeurope.com>
2016-06-01 11:08:18 +0200

    v1.4.2-RC1

--- dist/app/js/config.js
+++ dist/app/js/config.js
@@ -330,8 +330,8 @@ var config_data = {
         // Results per events page
         'page_results_events': 50,
         // ---------------------------------- Custom config for specifics app_type ---------------------------------- //
-        // Application type : default/popp/wd
-        'app_type': 'jb',
+        // Application type : default/popp/jb/wd
+        'app_type': 'default',
         // Config
         'custom_cfg': {
             'default': {
