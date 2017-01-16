6c04938ca830c84ea39d934fabeac8f555c5ee6e
Martin Vach <mv@zwaveeurope.com>
2017-01-16 15:07:59 +0100

    v1.6.0-RC14

--- dist/app/js/icons.js
+++ dist/app/js/icons.js
@@ -1,4 +1,4 @@
-/* Copyright:  Z-Wave Europe GmbH, Created: 09-01-2017 14:12:46 */
+/* Copyright:  Z-Wave Europe GmbH, Created: 16-01-2017 15:06:41 */
 /**
  * @overview The object with default icons.
  * @author Martin Vach
