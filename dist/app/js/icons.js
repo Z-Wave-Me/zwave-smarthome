01ca6ed9f70dd234efe093d983febee7f8204682
Martin Vach <mv@zwaveeurope.com>
2017-03-23 16:30:58 +0100

    Release SmartHome UI 1.7.0-RC-9

--- dist/app/js/icons.js
+++ dist/app/js/icons.js
@@ -1,4 +1,4 @@
-/* Copyright:  Z-Wave Europe GmbH, Created: 17-03-2017 15:15:38 */
+/* Copyright:  Z-Wave Europe GmbH, Created: 23-03-2017 16:30:35 */
 /**
  * @overview The object with default icons.
  * @author Martin Vach
