b7ef573f9454b7a56d767fb2edee4e778803f393
Martin Vach <mv@zwaveeurope.com>
2017-04-26 12:10:42 +0200

    RSS demo init

--- app/controllers/rss.js
+++ app/controllers/rss.js
@@ -10,17 +10,44 @@
 myAppController.controller('RssController', function ($scope, dataFactory, dataService, _) {
     $scope.rss = {
         all: {},
-        find: {}
+        find: {},
+        alert: {message: false, status: 'is-hidden', icon: false}
     };
     $scope.moduleMediaUrl = $scope.cfg.server_url + $scope.cfg.api_url + 'load/modulemedia/';
     /**
      * Load rss feeds
      */
     $scope.loadRss = function () {
-        dataFactory.getApi('modules').then(function (response) {
+        $scope.loading = {status: 'loading-spin', icon: 'fa-spinner fa-spin', message: $scope._t('loading')};
+        dataFactory.getApiLocal('rss.json').then(function (response) {
+            if(_.size(response.data) < 1){
+                $scope.rss.alert = {message: $scope._t('no_news'), status: 'alert-warning', icon: 'fa-exclamation-circle'};
+                return;
+            }
 
-        }, function (error) {});
+            $scope.rss.all = response.data;
+        }, function (error) {
+            alertify.alertError($scope._t('error_load_data'));
+            //$scope.rss.alert = {message: $scope._t('no_internet_connection'), status: 'alert-warning', icon: 'fa-wifi'};
+        }).finally(function() {// Always execute this on both error and success
+            $scope.loading = false;
+        });
     };
     $scope.loadRss();
+
+    /**
+     * Read a rss feed
+     */
+    $scope.readRss = function (v,modal,$event) {
+        var findIndex = _.findIndex($scope.rss.all, {id: v.id});
+        if(findIndex === -1){
+           return;
+
+        }
+        angular.extend($scope.rss.all[findIndex],{read: true});
+        $scope.rss.find = v;
+        $scope.handleModal(modal, $event);
+       console.log($scope.rss.find)
+    };
 });
 
