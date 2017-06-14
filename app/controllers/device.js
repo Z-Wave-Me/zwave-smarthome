3d568244e1e4b413e01bda66442dd61234d7cb8a
Michael Hensche <mh@zwave.eu>
2017-06-14 16:50:20 +0200

    rf433 minor fix

--- app/controllers/device.js
+++ app/controllers/device.js
@@ -26,26 +26,25 @@ myAppController.controller('DeviceController', function($scope, dataFactory) {
     $scope.loadperipheralsModules = function() {
         dataFactory.getApi('instances',false,true).then(function(response) {
             var EnOcean_module = _.findWhere(response.data.data,{moduleId:'EnOcean'});
-            if(!EnOcean_module){
-                return;
+            if(EnOcean_module){
+                $scope.enocean.installed = true;
+                if (!EnOcean_module.active) {
+                    $scope.enocean.alert = {message: $scope._t('enocean_not_active'), status: 'alert-warning', icon: 'fa-exclamation-circle'};
+                    return;
+                }
+                $scope.enocean.active = true;
             }
-            $scope.enocean.installed = true;
-            if (!EnOcean_module.active) {
-                $scope.enocean.alert = {message: $scope._t('enocean_not_active'), status: 'alert-warning', icon: 'fa-exclamation-circle'};
-                return;
-            }
-            $scope.enocean.active = true;
 
             var RF433_module = _.findWhere(response.data.data,{moduleId:'RF433'});
-            if(!RF433_module){
-                return;
+            if(RF433_module){
+                $scope.rf433.installed = true;
+                if (!RF433_module.active) {
+                    $scope.rf433.alert = {message: $scope._t('rf433_not_active'), status: 'alert-warning', icon: 'fa-exclamation-circle'};
+                    return;
+                }
+                $scope.rf433.active = true;
             }
-            $scope.rf433.installed = true;
-            if (!RF433_module.active) {
-                $scope.rf433.alert = {message: $scope._t('rf433_not_active'), status: 'alert-warning', icon: 'fa-exclamation-circle'};
-                return;
-            }
-            $scope.rf433.active = true;
+
         });
     };
 
