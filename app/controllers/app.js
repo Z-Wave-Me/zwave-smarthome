5a6f697057df1905ccd38d92809a040a3f12af02
Martin Vach <mv@zwaveeurope.com>
2017-09-04 15:45:00 +0200

    Revert "Fixing count of the updated apps"
    This reverts commit 8cd08f8c9b27a1d4ac9398ce40fd34660ac434b8.

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -280,7 +280,6 @@ myAppController.controller('AppBaseController', function ($scope, $rootScope, $f
                     }
 
                 }
-               
                 // Hides video item
                 if (item.category === 'surveillance') {
                     $scope.dataHolder.modules.cameraIds.push(item.id);
@@ -322,12 +321,8 @@ myAppController.controller('AppBaseController', function ($scope, $rootScope, $f
                     if (item.featured) {
                         $scope.dataHolder.modules.featured.push(item);
                     }
-                   
+
                     return items;
-                }else{
-                     console.log('Is hidden: ', item.id)
-                     console.log($scope.dataHolder.onlineModules.ids[item.id])
-                     
                 }
             });
 
