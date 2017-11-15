436afcaafb2011d66a285b8a0f9c42596741367c
Martin Vach <mv@zwaveeurope.com>
2017-11-15 15:32:04 +0100

    Finalyzing Automation tabs

--- app/controllers/automation.js
+++ app/controllers/automation.js
@@ -3,20 +3,20 @@
  * @author Martin Vach
  */
 
- /**
+/**
  * Aoutomation parent controller
  * @class AutomationController
  */
-myAppController.controller('AutomationController', function ($scope, $routeParams,cfg, dataFactory, dataService, _,myCache) {
+myAppController.controller('AutomationController', function ($scope,  $routeParams,$location, cfg, dataFactory, dataService, _, myCache) {
     $scope.automation = {
-        cfg:{
-            allowedName: ['LightScene','IfThen','ScheduledScene'],
-            imgPath:  cfg.server_url + cfg.api_url + 'load/modulemedia/'
-        },
+        moduleId: $routeParams.moduleId,
+        allowedIds: ['LightScene', 'IfThen', 'ScheduledScene'],
+        imgPath: cfg.server_url + cfg.api_url + 'load/modulemedia/',
+        state: '',
         localModules: {},
         instances: {
-            all:[],
-            state: 'success'
+            all: []
+           
         }
     };
 
@@ -27,40 +27,54 @@ myAppController.controller('AutomationController', function ($scope, $routeParam
     $scope.loadLocalModules = function () {
         dataFactory.getApi('modules').then(function (response) {
             // Get info from module
-            _.filter(response.data.data,function(v){
-                if($scope.automation.cfg.allowedName.indexOf(v.moduleName) > -1){
-                  $scope.automation.localModules[v.moduleName] = {
-                     version: v.version,
-                     icon:$scope.automation.cfg.imgPath + v.id + '/' + v.icon,
-                     singleton: v.singleton,
-                     title:v.defaults.title
-                 };
-                     
+            _.filter(response.data.data, function (v) {
+                if ($scope.automation.allowedIds.indexOf(v.moduleName) > -1) {
+                    $scope.automation.localModules[v.moduleName] = {
+                        version: v.version,
+                        icon: $scope.automation.imgPath + v.id + '/' + v.icon,
+                        singleton: v.singleton,
+                        title: v.defaults.title
+                    };
+
                 }
- 
-             });
+
+            });
         });
     };
-    $scope.loadLocalModules();
+    
 
-     /**
+    /**
      * Load instances
      * @returns {undefined}
      */
     $scope.loadInstances = function (moduleId) {
-        dataFactory.getApi('instances',null,true).then(function (response) {
-             $scope.automation.instances.all = _.where(response.data.data, {moduleId: moduleId});
+        dataFactory.getApi('instances', null, true).then(function (response) {
+            $scope.automation.instances.all = _.where(response.data.data, {
+                moduleId: moduleId
+            });
+            if(!_.size($scope.automation.instances.all)){
+                $scope.automation.state = 'blank';
+                return;
+            }
+            $scope.automation.state = 'success';
         }, function (error) {
             alertify.alertError($scope._t('error_load_data'));
         });
     };
+    if ($scope.automation.allowedIds.indexOf($routeParams.moduleId) > -1) {
+        $scope.loadLocalModules();
+        $scope.loadInstances($routeParams.moduleId);
+    }else{
+        $location.path('/error403');
+    }
 
-     /**
+
+    /**
      * Activate instance
      */
     $scope.activateInstance = function (input, activeStatus) {
         input.active = activeStatus;
-         if (input.id) {
+        if (input.id) {
             dataFactory.putApi('instances', input.id, input).then(function (response) {
                 $scope.loading = false;
 
@@ -78,7 +92,9 @@ myAppController.controller('AutomationController', function ($scope, $routeParam
     $scope.deleteInstance = function (input, message) {
         alertify.confirm(message, function () {
             dataFactory.deleteApi('instances', input.id).then(function (response) {
-                dataService.showNotifier({message: $scope._t('delete_successful')});
+                dataService.showNotifier({
+                    message: $scope._t('delete_successful')
+                });
                 $scope.reloadData();
             }, function (error) {
                 alertify.alertError($scope._t('error_delete_data'));
@@ -88,29 +104,3 @@ myAppController.controller('AutomationController', function ($scope, $routeParam
     };
 
 });
-
-/**
- * The controller that handles scenes.
- * @class AutomationSceneController
- */
-myAppController.controller('AutomationSceneController', function ($scope, cfg, dataFactory, dataService, _,myCache) {
-    $scope.loadInstances('LightScene');
-
-});
-
-/**
-* The controller that handles rules.
-* @class AutomationRuleController
-*/
-myAppController.controller('AutomationRuleController', function ($scope, cfg, dataFactory, dataService, _,myCache) {
-    $scope.loadInstances('IfThen');
-});
-
-/**
-* The controller that handles schedules.
-* @class AutomationScheduleController
-*/
-myAppController.controller('AutomationScheduleController', function ($scope, cfg, dataFactory, dataService, _,myCache) {
-    $scope.loadInstances('ScheduledScene');
-    });
-
