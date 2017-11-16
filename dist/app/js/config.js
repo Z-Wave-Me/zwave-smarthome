fd12b9fb69c34683ad3c5b9de0eb9ad5970655de
Martin Vach <mv@zwaveeurope.com>
2017-11-16 15:04:08 +0100

    Release SmartHome UI 1.8.0-RC-5

--- dist/app/js/config.js
+++ dist/app/js/config.js
@@ -12,9 +12,9 @@ var config_data = {
         //Application name
         'app_name': 'SmartHome UI',
         // Application version
-        'app_version': '1.8.0-RC-4',
+        'app_version': '1.8.0-RC-5',
         // Application (DIST) built date
-        'app_built': '16-11-2017 15:01:42',
+        'app_built': '16-11-2017 15:03:47',
         // Application ID
         'app_id': 'SmartHomeUI',
         // Server base url
--- dist/app/js/icons.js
+++ dist/app/js/icons.js
@@ -1,4 +1,4 @@
-/* Copyright:  Z-Wave Europe GmbH, Created: 16-11-2017 15:01:42 */
+/* Copyright:  Z-Wave Europe GmbH, Created: 16-11-2017 15:03:47 */
 /**
  * @overview The object with default icons.
  * @author Martin Vach
