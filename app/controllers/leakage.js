0b8e5cfeb7ba602e1e1b0601631ce45f1ab5ff25
Martin Vach <mv@zwaveeurope.com>
2018-03-20 16:15:30 +0100

    Leakage: loading data and adding views

--- app/controllers/leakage.js
+++ app/controllers/leakage.js
@@ -12,16 +12,12 @@ myAppController.controller('LeakageController', function ($scope, $routeParams, 
     state: '',
     enableTest: [],
   }
-  // TODO: Get route segment
-  console.log($location.path());
-
   /**
    * Load instances
    * @returns {undefined}
    */
   $scope.loadInstances = function () {
     dataFactory.getApi('instances', null, true).then(function (response) {
-      console.log(response.data.data)
       $scope.leakages.all = _.chain(response.data.data).flatten().where({
         moduleId: $scope.leakages.moduleId
       }).filter(function (v) {
@@ -76,7 +72,7 @@ myAppController.controller('LeakageController', function ($scope, $routeParams, 
     input.id = 0;
     input.title = input.title + ' - copy';
     dataFactory.postApi('instances', input).then(function (response) {
-      $location.path('/leakages/' + response.data.data.id);
+      $location.path($location.path() + '/' + response.data.data.id);
     }, function (error) {
       alertify.alertError($scope._t('error_update_data'));
     });
@@ -102,8 +98,47 @@ myAppController.controller('LeakageController', function ($scope, $routeParams, 
  * Controller that handles a leakage detail
  * @class LeakageIdController
  */
-myAppController.controller('LeakageIdController', function ($scope, $routeParams, $location, $timeout, cfg, dataFactory, dataService, _, myCache) {
+myAppController.controller('LeakageIdController', function ($scope, $routeParams, $location, $timeout, $filter, cfg, dataFactory, dataService, _, myCache) {
   $scope.leakage = {
+    sensors:['flood','alarm_flood','alarmSensor_flood'],
+    notifiers:['notification'],
+    interval: [60,120,300,600,900,1800,3600],
+    availableSensors: {},
+    availableDevices: {},
+    availableNotifiers: {},
+    devicesInRoom: [],
+    assignedSensors: [],
+    assignedDevices: [],
+    assignedNotifiers: [],
+    devicesInRoom:[],
+    rooms: [],
+      
+    cfg: {
+      switchBinary: {
+        filter: 'switchBinary',
+        level: ['on', 'off'],
+        default: {
+          device: '',
+          status: 'on'
+        }
+      },
+      switchMultilevel: {
+        filter: 'switchMultilevel',
+        level: ['on', 'off'],
+        operator: ['=', '!=', '>', '>=', '<', '<='],
+        default: {
+          device: '',
+          status: 'on'
+        }
+      },
+      toggleButton: {
+        filter: 'scene',
+        default: {
+          device: ''
+        }
+      }
+      
+    },
     input: {
       instanceId: $routeParams.id,
       moduleId: "LeakageProtection",
@@ -130,6 +165,20 @@ myAppController.controller('LeakageIdController', function ($scope, $routeParams
         active: instance.active,
         params: instance.params
       });
+       // Set assigned sensors
+       angular.forEach(instance.params.sensors, function (v) {
+        if(v !==''){
+         $scope.leakage.assignedSensors.push(v);
+        }
+      });
+
+       // Set assigned devices
+       angular.forEach(instance.params.action, function (v, k) {
+         var deviceId = $filter('hasNode')(v, v.filter + '.device');
+         if(deviceId){
+          $scope.leakage.assignedDevices.push(deviceId);
+        }
+      });
 
     }, function (error) {
       alertify.alertError($scope._t('error_load_data'));
@@ -141,13 +190,62 @@ myAppController.controller('LeakageIdController', function ($scope, $routeParams
     $scope.loadInstance($routeParams.id);
   }
 
+   /**
+   * Load rooms
+   */
+  $scope.loadRooms = function () {
+    dataFactory.getApi('locations').then(function (response) {
+      $scope.leakage.rooms = dataService.getRooms(response.data.data).indexBy('id').value();
+      $scope.loadDevices($scope.leakage.rooms);
+    });
+
+  };
+  $scope.loadRooms();
+
+  /**
+   * Load devices
+   */
+  $scope.loadDevices = function (rooms) {
+    dataFactory.getApi('devices').then(function (response) {
+      var devices = dataService.getDevicesData(response.data.data.devices);
+
+      _.filter(devices.value(),function(v){
+        var obj = {
+          deviceId: v.id,
+          deviceName: v.metrics.title,
+          deviceType: v.deviceType,
+          probeType: v.probeType,
+          location: v.location,
+          locationName: rooms[v.location].title
+        };
+        // Set sensors
+       if($scope.leakage.sensors.indexOf(v.probeType) > -1) {
+        $scope.leakage.availableSensors[v.id] =obj;
+       }
+         // Set devices
+         if(_.keys($scope.leakage.cfg).indexOf(v.deviceType) > -1) {
+          $scope.leakage.availableDevices[v.id] =obj;
+        }
+         // Set notifiers
+       if($scope.leakage.notifiers.indexOf(v.probeTyp) > -1) {
+        $scope.leakage.availableNotifiers[v.id] =obj;
+      }
+      });
+      // Set devices in the room
+      $scope.leakage.devicesInRoom = _.countBy($scope.leakage.availableDevices, function (v) {
+        return v.location;
+      });
+    }, function (error) {});
+  };
+
   /**
    * Store schedule
    */
   $scope.storeInstance = function (input, redirect) {
+    console.log(input)
     dataFactory.storeApi('instances', parseInt(input.instanceId, 10), input).then(function (response) {
       if (redirect) {
-        $location.path(redirect);
+        $location.path('/' + dataService.getUrlSegment($location.path()));
       }
 
     }, function (error) {
