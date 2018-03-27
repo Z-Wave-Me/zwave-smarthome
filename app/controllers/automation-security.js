f0caf91b85f301ca9c8ee5dbc2399c857d5491a1
Martin Vach <mv@zwaveeurope.com>
2018-03-27 16:05:53 +0200

    Security: Updating

--- app/controllers/automation-security.js
+++ app/controllers/automation-security.js
@@ -8,7 +8,7 @@
  */
 myAppController.controller('SecurityController', function ($scope, $routeParams, $location, $timeout, cfg, dataFactory, dataService, _, myCache) {
   $scope.security = {
-    moduleId: 'SecurityMode',
+    moduleId: 'SecurityModule',
     state: '',
     enableTest: [],
   }
@@ -96,73 +96,50 @@ myAppController.controller('SecurityController', function ($scope, $routeParams,
 
 /**
  * Controller that handles a security detail
- * @class LeakageIdController
+ * @class SecurityIdController
  */
 myAppController.controller('SecurityIdController', function ($scope, $routeParams, $location, $timeout, $filter, cfg, dataFactory, dataService, _, myCache) {
   $scope.security = {
-    sensors: ['flood', 'alarm_flood', 'alarmSensor_flood'],
-    devices: ['switchBinary', 'switchMultilevel', 'toggleButton'],
-    notifiers: ['notification_email','notification_push'],
-    interval: [60, 120, 300, 600, 900, 1800, 3600],
-    firedOn: ['on','off','alarm','revert'],
-    availableSensors: {},
-    availableDevices: {},
-    availableNotifiers: {},
-    devicesInRoom: [],
-    assignedDevices: [],
-    assignedNotifiers: [],
-    devicesInRoom: [],
-    rooms: [],
-
-    cfg: {
-      switchBinary: {
-        status: ['on', 'off'],
-        default: {
-          deviceId: '',
-          deviceType:'switchBinary',
-          status: 'on',
-          sendAction:false
-        }
-      },
-      switchMultilevel: {
-        status: ['on', 'off','lvl'],
-        min: 0,
-        max: 99,
-        operator: ['=', '!=', '>', '>=', '<', '<='],
-        default: {
-          deviceId: '',
-          deviceType:'switchMultilevel',
-          status: 'on',
-          level:0,
-          sendAction:false
-        }
-      },
-      toggleButton: {
-        default: {
-          deviceId: '',
-          deviceType:'toggleButton',
-          status: '',
-        }
-      },
-      notification: {
-        default: {
-          target: '',
-          message: '',
-          firedOn: 'alarm'
-        }
-      }
-
-    },
+    tab: 1,
+    cfg: {},
     input: {
       instanceId: $routeParams.id,
-      moduleId: "SecurityMode",
+      moduleId: "SecurityModule",
       active: true,
       title: "",
       params: {
-        sensors: [],
-        triggerEvent:[],
-        sendNotifications:[],
-        notificationsInterval: 0
+        devices: [],
+        times: {
+          aktive: false,
+          start: 10,
+          interval: 1,
+          silent: 0,
+          table: []
+        },
+        time: '00:00',
+        input:  [],
+        silentAlarms: {
+          table: [],
+          notification: {}
+        },
+        alarms: {
+          table: [],
+          notification: {}
+        },
+        armConfirm: {
+          table: [],
+          notification: {}
+        },
+        disarmConfirm: {
+          table: [],
+          notification: {}
+        },
+        clean: {
+          table: [],
+          notification: {}
+        },
+        controls: []
+
       },
     }
   };
@@ -173,9 +150,9 @@ myAppController.controller('SecurityIdController', function ($scope, $routeParam
   $scope.orig = {
     options: {}
   };
-  $scope.orig.options = angular.copy($scope.leakage.cfg);
+  $scope.orig.options = angular.copy($scope.security.cfg);
   $scope.resetOptions = function () {
-   $scope.leakage.cfg = angular.copy($scope.orig.options);
+    $scope.security.cfg = angular.copy($scope.orig.options);
 
   };
 
@@ -185,7 +162,7 @@ myAppController.controller('SecurityIdController', function ($scope, $routeParam
   $scope.loadInstance = function (id) {
     dataFactory.getApi('instances', '/' + id, true).then(function (instances) {
       var instance = instances.data.data;
-      angular.extend($scope.leakage.input, {
+      angular.extend($scope.security.input, {
         title: instance.title,
         active: instance.active,
         params: instance.params
@@ -193,14 +170,14 @@ myAppController.controller('SecurityIdController', function ($scope, $routeParam
       // Set assigned devices
       angular.forEach(instance.params.triggerEvent, function (v, k) {
         if (v.deviceId) {
-          $scope.leakage.assignedDevices.push(v.deviceId);
+          $scope.security.assignedDevices.push(v.deviceId);
         }
       });
 
       // Set assigned devices
       angular.forEach(instance.params.sendNotifications, function (v, k) {
         if (v.notifier) {
-          $scope.leakage.assignedNotifiers.push(v.notifier);
+          $scope.security.assignedNotifiers.push(v.notifier);
         }
       });
 
@@ -219,8 +196,8 @@ myAppController.controller('SecurityIdController', function ($scope, $routeParam
    */
   $scope.loadRooms = function () {
     dataFactory.getApi('locations').then(function (response) {
-      $scope.leakage.rooms = dataService.getRooms(response.data.data).indexBy('id').value();
-      $scope.loadDevices($scope.leakage.rooms);
+      $scope.security.rooms = dataService.getRooms(response.data.data).indexBy('id').value();
+      $scope.loadDevices($scope.security.rooms);
     });
 
   };
@@ -242,112 +219,10 @@ myAppController.controller('SecurityIdController', function ($scope, $routeParam
           location: v.location,
           locationName: rooms[v.location].title
         };
-        // Set sensors
-        if ($scope.leakage.sensors.indexOf(v.probeType) > -1) {
-          $scope.leakage.availableSensors[v.id] = obj;
-        }
-        // Set devices
-        if ($scope.leakage.devices.indexOf(v.deviceType) > -1) {
-          $scope.leakage.availableDevices[v.id] = obj;
-        }
-        // Set notifiers
-        if ($scope.leakage.notifiers.indexOf(v.probeType) > -1) {
-          $scope.leakage.availableNotifiers[v.id] = obj;
-        }
-      });
-      // Set devices in the room
-      $scope.leakage.devicesInRoom = _.countBy($scope.leakage.availableDevices, function (v) {
-        return v.location;
       });
     }, function (error) {});
   };
 
-  /**
-   * Assign sensor
-   * @param {int} sensorId
-   * @returns {undefined}
-   */
-  $scope.assignSensor = function (sensorId) {
-    if ($scope.leakage.input.params.sensors.indexOf(sensorId) === -1) {
-      $scope.leakage.input.params.sensors.push(sensorId);
-    }
-
-  };
-
-  /**
-   * Remove sensor id from assigned sensors
-   * @param {string} deviceId 
-   */
-  $scope.unassignSensor = function (deviceId) {
-    var deviceIndex = $scope.leakage.input.params.sensors.indexOf(deviceId);
-    if (deviceIndex > -1) {
-      $scope.leakage.input.params.sensors.splice(deviceIndex, 1);
-    }
-
-  };
-
-  /**
-   * Assign a device
-   * @param {object} device
-   * @returns {undefined}
-   */
-  $scope.assignDevice = function (device) {
-    var input = $scope.leakage.cfg[device.deviceType], obj = {};
-    if (!input || $scope.leakage.assignedDevices.indexOf(device.deviceId) > -1) {
-      return;
-    }
-    
-    /* obj['filter'] = input.filter;
-    obj[input.filter] = input.default;
-    obj[input.filter].device = device.deviceId; */
-    obj = input.default;
-    obj.deviceId = device.deviceId;
-    console.log(input.default)
-    $scope.leakage.input.params.triggerEvent.push(input.default);
-    $scope.leakage.assignedDevices.push(device.deviceId);
-    $scope.resetOptions();
-  };
-
-  /**
-   * Remove device id from assigned device
-   * @param {int} index 
-   * @param {string} deviceId 
-   */
-  $scope.unassignDevice = function (targetIndex, deviceId) {
-
-    var deviceIndex = $scope.leakage.assignedDevices.indexOf(deviceId);
-    if (targetIndex > -1) {
-      $scope.leakage.input.params.triggerEvent.splice(targetIndex, 1);
-      $scope.leakage.assignedDevices.splice(deviceIndex, 1);
-    }
-
-  };
-
-  /**
-   * Assign notification
-   * @param {object} notification
-   * @returns {undefined}
-   */
-  $scope.assignNotification = function (notification) {
-      $scope.leakage.input.params.sendNotifications.push(notification);
-      $scope.resetOptions();
-
-  };
-
-  /**
-   * Remove notification id from assigned notifications
-   * @param {int} index 
-   * @param {string} deviceId 
-   */
-  $scope.unassignNotification = function (targetIndex, deviceId) {
-
-    var deviceIndex = $scope.leakage.assignedNotifiers.indexOf(deviceId);
-    if (targetIndex > -1) {
-      $scope.leakage.input.params.sendNotifications.splice(targetIndex, 1);
-      $scope.leakage.assignedNotifiers.splice(deviceIndex, 1);
-    }
-
-  };
 
   /**
    * Store instance
