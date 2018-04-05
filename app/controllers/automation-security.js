aab3c85bfcc314c44748597c5198d5e1163e4435
Martin Vach <mv@zwaveeurope.com>
2018-04-05 16:20:19 +0200

    Security: Adding external control

--- app/controllers/automation-security.js
+++ app/controllers/automation-security.js
@@ -100,9 +100,25 @@ myAppController.controller('SecurityController', function ($scope, $routeParams,
  */
 myAppController.controller('SecurityIdController', function ($scope, $routeParams, $location, $timeout, $filter, cfg, dataFactory, dataService, _, myCache) {
   $scope.security = {
-    tab: 3,
+    tab: 2,
     days: [1, 2, 3, 4, 5, 6, 0],
+   /*  assigned: {
+      controls: []
+    }, */
+    devices:{
+      controls:[]
+    },
     cfg: {
+      controls:{
+        deviceType:['switchBinary'],
+        status: ['on', 'off','never'],
+        default:{
+          devices: '',
+          armCondition: 'never',
+          disarmCondition: 'never',
+          clearCondition: 'never'
+        }
+      },
       times:{
         default:{
           '0': false,
@@ -183,18 +199,11 @@ myAppController.controller('SecurityIdController', function ($scope, $routeParam
         params: instance.params
       });
       // Set assigned devices
-      angular.forEach(instance.params.triggerEvent, function (v, k) {
-        if (v.deviceId) {
-          $scope.security.assignedDevices.push(v.deviceId);
+     /*  angular.forEach(instance.params.controls.table, function (v, k) {
+        if (v.devices) {
+          $scope.security.assigned.controls.push(v.devices);
         }
-      });
-
-      // Set assigned devices
-      angular.forEach(instance.params.sendNotifications, function (v, k) {
-        if (v.notifier) {
-          $scope.security.assignedNotifiers.push(v.notifier);
-        }
-      });
+      }); */
 
     }, function (error) {
       alertify.alertError($scope._t('error_load_data'));
@@ -224,7 +233,6 @@ myAppController.controller('SecurityIdController', function ($scope, $routeParam
   $scope.loadDevices = function (rooms) {
     dataFactory.getApi('devices').then(function (response) {
       var devices = dataService.getDevicesData(response.data.data.devices);
-
       _.filter(devices.value(), function (v) {
         var obj = {
           deviceId: v.id,
@@ -232,10 +240,60 @@ myAppController.controller('SecurityIdController', function ($scope, $routeParam
           deviceType: v.deviceType,
           probeType: v.probeType,
           location: v.location,
-          locationName: rooms[v.location].title
+          locationName: rooms[v.location].title,
+          iconPath: v.iconPath
         };
+         // Set controls
+      if ($scope.security.cfg.controls.deviceType.indexOf(v.deviceType) > -1) {
+        //$scope.security.devices.all[v.id] = obj;
+        $scope.security.devices.controls.push(obj);
+      }
       });
     }, function (error) {});
+  };
+  
+  /**
+   * TODO: Create function for all input/models
+   * Get model index by device ID
+   * @param {string} deviceId
+   * @returns {undefined}
+   */
+  $scope.getModelIndex = function (deviceId,node) {
+    //var index = $scope.security.input.params.controls.table.indexOf(deviceId);
+    //var index = _.findIndex($scope.security.input.params.controls.table, { devices: deviceId });
+    var index = _.findIndex($filter('hasNode')($scope.security.input.params,node), { devices: deviceId });
+    return index;
+  };
+   //////////External control////////// 
+  /**
+   * Assign an external control
+   * @param {string} deviceId
+   * @returns {undefined}
+   */
+  $scope.assignExternalControl = function (deviceId) {
+    var input = $scope.security.cfg.controls.default;
+    var deviceIndex =  _.findIndex($scope.security.input.params.controls.table, { devices: deviceId });
+   if (deviceIndex > -1) {
+      return;
+    }
+   input.devices = deviceId;
+    //$scope.security.assigned.controls.push(deviceId);
+    $scope.security.input.params.controls.table.push(input);
+    $scope.resetOptions();
+  };
+  /**
+   * Unassign an external control
+   * @param {string} deviceId
+   * @returns {undefined}
+   */
+  $scope.unassignExternalControl = function (deviceId) {
+    //var assignedIndex = $scope.security.assigned.controls.indexOf(deviceId);
+    var deviceIndex = _.findIndex($scope.security.input.params.controls.table, { devices: deviceId });
+    if (deviceIndex > -1) {
+     // $scope.security.assigned.controls.splice(assignedIndex, 1);
+      $scope.security.input.params.controls.table.splice(deviceIndex, 1);
+     
+    }
   };
   ////////// Advanced schedule ////////// 
   /**
