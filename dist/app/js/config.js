ea64e1a29cd6f40c345547fb4cd7265ff9b3f465
Niels Roche <nroche.z-wave@outlook.de>
2018-06-26 14:06:45 +0200

    DIST 1.9.0 - 26.06.18

--- dist/app/js/config.js
+++ dist/app/js/config.js
@@ -14,7 +14,7 @@ var config_data = {
 		// Application version
 		'app_version': '1.9.0',
 		// Application (DIST) built date
-		'app_built': '22-06-2018 12:26:55',
+		'app_built': '26-06-2018 13:38:40',
 		// Application ID
 		'app_id': 'SmartHomeUI',
 		// Server base url
--- dist/app/js/icons.js
+++ dist/app/js/icons.js
@@ -1,4 +1,4 @@
-/* Copyright:  Z-Wave Europe GmbH, Created: 22-06-2018 12:26:55 */
+/* Copyright:  Z-Wave Europe GmbH, Created: 26-06-2018 13:38:39 */
 /**
  * @overview The object with default icons.
  * @author Martin Vach
