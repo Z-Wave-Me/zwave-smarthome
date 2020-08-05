a96579b92910ca87655b32424bb69a071708be3c
Poltorak Serguei <ps@z-wave.me>
2020-08-05 05:45:41 +0300

    dist

--- dist/app/js/config.js
+++ dist/app/js/config.js
@@ -14,7 +14,7 @@ var config_data = {
 		// Application version
 		'app_version': '1.10.2',
 		// Application (DIST) built date
-		'app_built': '04-08-2020 19:54:33',
+		'app_built': '05-08-2020 05:12:23',
 		// Application ID
 		'app_id': 'SmartHomeUI',
 		// Server base url
@@ -150,8 +150,9 @@ var config_data = {
 			'remove_dsk_collection': 'JS/Run/saveObject("zwaydskCollection",null)', // Remove DSK collection
 			'enable_smart_start': 'JS/Run/zway.SmartStartEnable()', // enable SmartStart
 			'locations_image': 'ZAutomation/api/v1/locations/image', // delete cutom room image // TODO: add backend,
-			'notificationFiltering': 'ZAutomation/api/v1/notificationFiltering', // get and set NotificationFiltering settings app for a user
-			'remove_app': 'MobileAppSupportAPI/app' // remove app
+			'notification_filtering': 'ZAutomation/api/v1/notificationFiltering', // get and set NotificationFiltering settings app for a user
+			'notification_channels': 'ZAutomation/api/v1/notificationChannels', // get NotificationFiltering channels for the user
+			'mobile_app_support': 'MobileAppSupportAPI/app' // MobileAppSupport
 		},
 		// List of remote api URLs
 		'api_remote': {
