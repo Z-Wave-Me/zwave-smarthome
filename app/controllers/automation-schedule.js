ff49c768e9d059e8711619cf60da1fad1fb7ade1
Poltorak Serguei <ps@z-wave.me>
2020-12-21 17:44:11 +0300

    Fixed Automation Scene/Rule/Schedule bug due to empty notification

--- app/controllers/automation-schedule.js
+++ app/controllers/automation-schedule.js
@@ -396,12 +396,14 @@ myAppController.controller('AutomationScheduleIdController', function($scope, $r
 	$scope.loadNotificationChannels = function(rooms) {
 		dataFactory.getApi('notification_channels', '/all').then(function(response) {
 			$scope.notifications.channels = response.data.data;
-			$scope.schedule.input.params.notifications.forEach(function(n) {
-				var nc = _.findWhere($scope.notifications.channels, { id: n.target });
-				if (nc) {
-					n.targetName = nc.name;
-				}
-			});
+			if ($scope.schedule.input.params.notifications) {
+				$scope.schedule.input.params.notifications.forEach(function(n) {
+					var nc = _.findWhere($scope.notifications.channels, { id: n.target });
+					if (nc) {
+						n.targetName = nc.name;
+					}
+				});
+			}
 		}, function(error) {});
 	};
 	$scope.loadNotificationChannels();
