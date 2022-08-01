715860b01945ae9fe7658cce133e09101a8921fb
Xel4ek <l-baal@yandex.ru>
2022-08-01 19:56:42 +0300

    refactor: increase Inclusion/Exclusion timeout to 180 seconds

--- app/controllers/zwave-inclusion.js
+++ app/controllers/zwave-inclusion.js
@@ -13,7 +13,7 @@ myAppController.controller('ZwaveInclusionController', function ($scope, $q, $ro
         cfg: {
             checkInterviewTimeout: 3000, // miliseconds
             checkInterviewRepeat: 16, // times
-            inexTimeout: 120000 // Inclusion/Exclusion timeout - miliseconds
+            inexTimeout: 180000 // Inclusion/Exclusion timeout - miliseconds
         },
         device: {
             hasBattery: false,
