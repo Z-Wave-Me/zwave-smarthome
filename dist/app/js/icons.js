50dbfc6ebdda6fe2595664c489b33c8999b0462f
Poltorak Serguei <ps@z-wave.me>
2024-10-10 18:04:32 +0300

    dist

--- dist/app/js/icons.js
+++ dist/app/js/icons.js
@@ -1,4 +1,4 @@
-/* Copyright:  Z-Wave.Me, Created: 10-10-2024 17:03:24 */
+/* Copyright:  Z-Wave.Me, Created: 10-10-2024 18:04:31 */
 /**
  * @overview The object with default icons.
  * @author Martin Vach
